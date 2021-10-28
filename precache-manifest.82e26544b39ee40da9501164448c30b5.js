d6a4e08abebf36b01a13f16af3c4d7e36293fffd
Suchlikesamsung <93254567+Suchlikesamsung@users.noreply.github.com>
2021-10-28 21:05:21 +0900

    Add files via upload

---
+++ precache-manifest.82e26544b39ee40da9501164448c30b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aa6a735e2ca6b41d7662b845befa8c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77f6e9987d4c4f367162",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "52e434338ad7902e1394",
+    "url": "/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "77f6e9987d4c4f367162",
+    "url": "/static/js/2.09497131.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.09497131.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52e434338ad7902e1394",
+    "url": "/static/js/main.ad94c83a.chunk.js"
+  },
+  {
+    "revision": "9d56b0ea2705241215be",
+    "url": "/static/js/runtime-main.cf8c75f0.js"
+  }
+]);
